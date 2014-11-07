6dc2c8aec11615e8d96258ac84432fcfe7926c89
Régis Gaidot <rgaidot@gmail.com>
2014-11-07 18:08:20 +0100

    fix  tar: Unrecognized archive format  tar: Error exit delayed from previous errors.

--- boostrap-prestashop.sh
+++ boostrap-prestashop.sh
@@ -7,5 +7,5 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-curl http://www.prestashop.com/download/old/prestashop_1.6.0.9.zip | tar -xvzf -
+curl -L http://www.prestashop.com/download/old/prestashop_1.6.0.9.zip | tar -xvzf -
 mv prestashop $1 ; cp config/* $1 ; mv $1/settings.inc.php $1/config/ ; cd $1 ; fig up
